e8a49c8f58753457aefc09e6a080993e13663bd2
nyefan <git@nyefan.org>
2026-07-06 13:55:10 -0700

    test: email subscription POST

--- ztpir/tests/health_check.rs
+++ ztpir/tests/health_check.rs
@@ -1,3 +1,5 @@
+use reqwest::StatusCode;
+use reqwest::header::CONTENT_TYPE;
 use std::net::TcpListener;
 
 fn spawn_app() -> String {
@@ -15,6 +17,7 @@ fn spawn_app() -> String {
 async fn health_check_returns_ok() {
     let address = spawn_app();
     let client = reqwest::Client::new();
+
     let response = client
         .get(&format!("{address}/healthz"))
         .send()
@@ -22,4 +25,48 @@ async fn health_check_returns_ok() {
         .expect("Failed to send request");
 
     assert!(response.status().is_success());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // TODO: urlencode with lib
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{address}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    assert_eq!(StatusCode::OK, response.status())
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (body, error_description) in cases {
+        let response = client
+            .post(&format!("{address}/subscriptions"))
+            .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            StatusCode::BAD_REQUEST,
+            response.status(),
+            "The API did not fail with 400 Bad Request when the payload was {error_description}"
+        );
+    }
 }
