dc7b9a2c4e72d0dcd74100481e74bdbf7a3b5301
nyefan <git@nyefan.org>
2026-07-30 17:42:07 -0700

    refactor: use actix_web::Error for subscription error handling

--- app/src/routes/subscriptions/subscribe.rs
+++ app/src/routes/subscriptions/subscribe.rs
@@ -1,12 +1,16 @@
+use std::error::Error;
 use crate::domain::{
     NewSubscriber, SubscriberConfirmationToken, SubscriberEmail, SubscriberName, SubscriptionStatus,
 };
 use crate::email_client::EmailClient;
 use crate::startup::ApplicationBaseUrl;
-use actix_web::{HttpResponse, web};
+use actix_web::error::ErrorInternalServerError;
+use actix_web::http::StatusCode;
+use actix_web::{HttpResponse, ResponseError, web};
 use rand::distr::Alphanumeric;
 use rand::{RngExt, rng};
 use sqlx::{PgPool, Postgres, Transaction};
+use std::fmt::{Display, Formatter};
 use tracing::instrument;
 use uuid::Uuid;
 
@@ -16,13 +20,13 @@ pub(crate) struct FormData {
     email: String,
     name: String,
 }
-
 impl TryFrom<FormData> for NewSubscriber {
-    type Error = String;
+    type Error = actix_web::error::Error;
 
     fn try_from(form: FormData) -> Result<Self, Self::Error> {
-        let name = SubscriberName::parse(form.name)?;
-        let email = SubscriberEmail::parse(form.email)?;
+        let name = SubscriberName::parse(form.name).map_err(actix_web::error::ErrorBadRequest)?;
+        let email =
+            SubscriberEmail::parse(form.email).map_err(actix_web::error::ErrorBadRequest)?;
         let confirmation_token = rng()
             .sample_iter(Alphanumeric)
             .map(char::from)
@@ -36,6 +40,77 @@ impl TryFrom<FormData> for NewSubscriber {
     }
 }
 
+#[derive(Debug)]
+pub struct InsertSubscriberError(sqlx::Error);
+impl InsertSubscriberError {
+    pub const ERROR_MESSAGE: &str =
+        "A database error was encountered while trying to create a new subscription.";
+}
+impl Display for InsertSubscriberError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Self::ERROR_MESSAGE)
+    }
+}
+impl ResponseError for InsertSubscriberError {}
+impl From<sqlx::Error> for InsertSubscriberError {
+    fn from(err: sqlx::Error) -> Self {
+        InsertSubscriberError(err)
+    }
+}
+impl Error for InsertSubscriberError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.0)
+    }
+}
+
+#[derive(Debug)]
+pub struct InsertSubscriberTokenError(sqlx::Error);
+impl InsertSubscriberTokenError {
+    pub const ERROR_MESSAGE: &str = "A database error was encountered while trying to create a subscription confirmation token.";
+}
+impl Display for InsertSubscriberTokenError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Self::ERROR_MESSAGE)
+    }
+}
+impl ResponseError for InsertSubscriberTokenError {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::BAD_GATEWAY
+    }
+}
+impl From<sqlx::Error> for InsertSubscriberTokenError {
+    fn from(err: sqlx::Error) -> Self {
+        InsertSubscriberTokenError(err)
+    }
+}
+impl Error for InsertSubscriberTokenError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.0)
+    }
+}
+
+#[derive(Debug)]
+#[expect(dead_code)] // we use this exclusively to print useful error logs, but debug is ignored by dead code analysis
+pub struct SendConfirmationEmailError(String);
+impl SendConfirmationEmailError {
+    pub const ERROR_MESSAGE: &str = "A database error was encountered while trying to create a subscription confirmation token.";
+}
+impl Display for SendConfirmationEmailError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", Self::ERROR_MESSAGE)
+    }
+}
+impl ResponseError for SendConfirmationEmailError {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::BAD_GATEWAY
+    }
+}
+impl From<String> for SendConfirmationEmailError {
+    fn from(err: String) -> Self {
+        SendConfirmationEmailError(err)
+    }
+}
+
 #[instrument(
     name = "New subscription request received",
     skip(form, pool, email_client, application_base_url),
@@ -46,48 +121,29 @@ pub(crate) async fn subscribe(
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
     application_base_url: web::Data<ApplicationBaseUrl>,
-) -> HttpResponse {
-    fn bad_request(err: String) -> HttpResponse {
-        tracing::warn!("Failed to parse form data: {:?}", err);
-        HttpResponse::BadRequest().body(err)
-    }
-    fn database_error(err: sqlx::Error) -> HttpResponse {
-        tracing::error!("Failed to insert into database: {:?}", err);
-        HttpResponse::InternalServerError().finish()
-    }
-    fn email_client_error(err: String) -> HttpResponse {
-        tracing::error!("Failed to send confirmation email: {:?}", err);
-        HttpResponse::InternalServerError().finish()
-    }
+) -> Result<HttpResponse, actix_web::Error> {
+    let subscriber = form.into_inner().try_into()?;
 
-    async {
-        let subscriber: NewSubscriber = form.into_inner().try_into().map_err(bad_request)?;
-
-        // should the transaction not commit until the email is sent?  long transaction = bad, but
-        // idempotency is not guaranteed by the compiler - how would we even represent that???
-        {
-            let mut transaction = pool.begin().await.map_err(database_error)?;
-            let subscriber_id = insert_subscriber(&mut transaction, &subscriber)
-                .await
-                .map_err(database_error)?;
-            insert_subscriber_confirmation_token(
-                &mut transaction,
-                &subscriber_id,
-                &subscriber.confirmation_token,
-            )
+    // should the transaction not commit until the email is sent?  long transaction = bad, but
+    // idempotency is not guaranteed by the compiler - how would we even represent that???
+    {
+        let mut transaction = pool.begin().await.map_err(ErrorInternalServerError)?;
+        let subscriber_id = insert_subscriber(&mut transaction, &subscriber).await?;
+        insert_subscriber_confirmation_token(
+            &mut transaction,
+            &subscriber_id,
+            &subscriber.confirmation_token,
+        )
+        .await?;
+        transaction
+            .commit()
             .await
-            .map_err(database_error)?;
-            transaction.commit().await.map_err(database_error)?;
-        }
-
-        send_confirmation_email(&email_client, &subscriber, &application_base_url)
-            .await
-            .map_err(email_client_error)?;
-
-        Ok(HttpResponse::Ok().finish())
+            .map_err(ErrorInternalServerError)?;
     }
-    .await
-    .unwrap_or_else(|err| err)
+
+    send_confirmation_email(&email_client, &subscriber, &application_base_url).await?;
+
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[instrument(
@@ -97,8 +153,8 @@ pub(crate) async fn subscribe(
 async fn insert_subscriber(
     transaction: &mut Transaction<'_, Postgres>,
     new_subscriber: &NewSubscriber,
-) -> Result<Uuid, sqlx::Error> {
-    sqlx::query!(
+) -> Result<Uuid, InsertSubscriberError> {
+    let id = sqlx::query!(
         r#"
             INSERT INTO subscriptions(email, name, status)
             VALUES($1, $2, $3::subscription_status)
@@ -110,18 +166,15 @@ async fn insert_subscriber(
     )
     .fetch_one(&mut **transaction)
     .await
-    .map(|result| result.id)
-    .map_err(|e| {
-        tracing::error!("Failed to insert subscriber: {:?}", e);
-        e
-    })
+    .map(|result| result.id)?;
+    Ok(id)
 }
 
 async fn insert_subscriber_confirmation_token(
     transaction: &mut Transaction<'_, Postgres>,
     subscriber_id: &Uuid,
     confirmation_token: &SubscriberConfirmationToken,
-) -> Result<(), sqlx::Error> {
+) -> Result<(), InsertSubscriberTokenError> {
     sqlx::query!(
         r#"
             INSERT INTO subscriptions_confirmation_tokens (subscriptions_id, token)
@@ -131,21 +184,15 @@ async fn insert_subscriber_confirmation_token(
         confirmation_token
     )
     .execute(&mut **transaction)
-    .await
-    .map(|_| {
-        tracing::debug!("Inserted subscription_token: {confirmation_token}");
-    })
-    .map_err(|e| {
-        tracing::error!("Failed to insert subscription_token: {:?}", e);
-        e
-    })
+    .await?;
+    Ok(())
 }
 
 async fn send_confirmation_email(
     email_client: &EmailClient,
     subscriber: &NewSubscriber,
     base_url: &ApplicationBaseUrl,
-) -> Result<(), String> {
+) -> Result<(), SendConfirmationEmailError> {
     let NewSubscriber {
         name,
         email,
@@ -163,7 +210,8 @@ async fn send_confirmation_email(
     );
     email_client
         .send_email(email, &subject, &html_body, &text_body)
-        .await
+        .await?;
+    Ok(())
 }
 
 // TODO: test the actual behavior of subscribe (i.e. that it inserts into the db, etc.)
